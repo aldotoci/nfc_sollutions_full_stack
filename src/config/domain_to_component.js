9476a4e27caff992a86e56cfef6141da9bfea115
unknown <tocialdo@gmail.com>
2023-11-23 18:43:52 +0100

    Added amazonic

--- src/config/domain_to_component.js
+++ src/config/domain_to_component.js
@@ -1,7 +1,11 @@
 import Artigiano from '../components/artigiano'
 import Yamato from '../components/yamato'
+import Aamazonic from '../components/amazonic'
+
 
 export default {
     'artigiano': Artigiano,
-    'yamato': Yamato
+    'yamato': Yamato,
+    'amazonic': Aamazonic
 }
+
